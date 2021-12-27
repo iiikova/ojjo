3a6de6341d54f5f7ab254db6ff42fcd48cf58a11
Ihor Koval <ikova.ua@gmail.com>
2021-12-27 19:22:29 +0200

    salon done

--- src/Components/OurSalon/OurSalon.jsx
+++ src/Components/OurSalon/OurSalon.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import s from "./OurSalon.module.scss";
 
 export default function OurSalon() {
   return (
-    <div>
-      <h3>Не знаете, что выбрать?</h3>
-      <h2>Посетите наши салоны в Москве</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut duis tortor
-        vitae pellentesque egestas quam pulvinar. Pellentesque porttitor velit
-        sit pellentesque. Suspendisse donec pretium id dignissim. Dignissim
-        ultrices eget orci viverra. Egestas quis et ut ultrices imperdiet lectus
-        nulla tempus. Pharetra lorem sem purus nisi libero viverra ipsum.
-      </p>
-      <button>наши салоны</button>
+    <div className={s.outer}>
+      <div className={s.inner}>
+        <h3>Не знаете, что выбрать?</h3>
+        <h2>Посетите наши салоны в Москве</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut duis
+          tortor vitae pellentesque egestas quam pulvinar. Pellentesque
+          porttitor velit sit pellentesque. Suspendisse donec pretium id
+          dignissim. Dignissim ultrices eget orci viverra. Egestas quis et ut
+          ultrices imperdiet lectus nulla tempus. Pharetra lorem sem purus nisi
+          libero viverra ipsum.
+        </p>
+        <button>наши салоны</button>
+      </div>
     </div>
   );
 }
