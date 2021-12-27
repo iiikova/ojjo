6635b0c1581047f0b6a61b16de9d26f8ea327669
Ihor Koval <ikova.ua@gmail.com>
2021-12-27 18:56:08 +0200

    secondScreen done

--- src/Components/SecondScreen/SecondScreen.jsx
+++ src/Components/SecondScreen/SecondScreen.jsx
@@ -1,28 +1,53 @@
-import React from 'react'
+import React from "react";
+import s from "./SecondScreen.module.scss";
 
 export default function SecondScreen() {
-    return (
-        <div>
-        <div>
-            <h3>К мероприятиям</h3>
-            <h2>Настоящая красота здесь!</h2>
+  return (
+    <div className={s.outer}>
+      <div className={s.inner}>
+        <div className={s.main}>
+          <h3>К мероприятиям</h3>
+          <h2>Настоящая красота здесь!</h2>
+          <div className={s.links}>
+            <div className={s.active}>Свадьба</div>
             <div>
-                <div>Свадьба</div>
-                <div>МУЖУ</div>
-                <div>жене</div>
-                <div>партнеру</div>
-                <div>коллекции</div>
-                <div>редкость</div>
+              <p>МУЖУ</p>
             </div>
             <div>
-                <div>Кольца</div>
-                <div>серьги</div>
-                <div>подвески</div>
-                <div>запонки</div>
-                <div>браслеты</div>
-                <div>часы</div>
+              <p>жене</p>
             </div>
+            <div>
+              <p>партнеру</p>
+            </div>
+            <div>
+              <p>коллекции</p>
+            </div>
+            <div>
+              <p>редкость</p>
+            </div>
+          </div>
+          <div className={s.category}>
+            <div>
+              <p>Кольца</p>
+            </div>
+            <div>
+              <p>серьги</p>
+            </div>
+            <div>
+              <p>подвески</p>
+            </div>
+            <div>
+              <p>запонки</p>
+            </div>
+            <div>
+              <p>браслеты</p>
+            </div>
+            <div>
+              <p>часы</p>
+            </div>
+          </div>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
