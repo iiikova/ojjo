890d918a239efaa4576f455367a2a639a5f4e28c
Ihor Koval <ikova.ua@gmail.com>
2021-12-29 17:28:10 +0200

    subscribe done

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React from "react";
+import s from "./Subscribe.module.scss";
 
 export default function Subscribe() {
-    return (
-        <div>
-       <div>
-           <h3>Полезные советы и персональный предложения</h3>
-           <h2>Эксклюзивная рассылка</h2>
-           <div>
-               <div>
-                   <p>Личный менеджер</p>
-                   <p>Доставка и оформление</p>
-                   <p>Индивидуальный дизайн</p>
-               </div>
-               <div>
-                   <div><input type="text" />input</div>
-                   <button>отправить</button>
-               </div>
-           </div>
-       </div>
+  return (
+    <div className={s.outer}>
+      <div className={s.inner}>
+        <h3>Полезные советы и персональный предложения</h3>
+        <h2>Эксклюзивная рассылка</h2>
+        <div className={s.subscribe}>
+          <div className={s.info}>
+            <p>
+              <span class="material-icons">play_arrow</span> Личный менеджер
+            </p>
+            <p>
+              <span class="material-icons">play_arrow</span> Доставка и
+              оформление
+            </p>
+            <p>
+              <span class="material-icons">play_arrow</span> Индивидуальный
+              дизайн
+            </p>
+          </div>
+          <div className={s.input_outer}>
+            <div className={s.input_inner}>
+              <div>
+                <input type="text" placeholder="Ваш e-mail" />
+              </div>
+              <button>отправить</button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
